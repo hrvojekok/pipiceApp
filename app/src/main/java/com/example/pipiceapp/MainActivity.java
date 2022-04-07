55510e583060b0829aa7decc298b797b038b72ca
Hrvoje Kokosarevic <hrvoje.kok@gmail.com>
2022-04-07 22:33:05 +0200

    dodan listView

--- app/src/main/java/com/example/pipiceapp/MainActivity.java
+++ app/src/main/java/com/example/pipiceapp/MainActivity.java
@@ -5,8 +5,12 @@ import static android.content.ContentValues.TAG;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -15,6 +19,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -22,21 +28,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(MainActivity.this, "Firebase connected", Toast.LENGTH_LONG).show();
+        TextView textView = findViewById(R.id.text1);
+        ListView listView = findViewById(R.id.listView);
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference reference = database.getReference("message");
+        ArrayList<String> arrayList = new ArrayList<>();
 
-        reference.setValue("Hello, world!");
+        arrayList.add("andorid");
+        arrayList.add("ando2rid");
+        arrayList.add("and3orid");
+        arrayList.add("ando24rid");
+        arrayList.add("ando45rid");
+        arrayList.add("andorid");
+        arrayList.add("ando2rid");
+        arrayList.add("and3orid");
+        arrayList.add("ando24rid");
+        arrayList.add("ando45rid");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList);
+        listView.setAdapter(arrayAdapter);
+
+
+        //Toast.makeText(MainActivity.this, "Firebase connected", Toast.LENGTH_LONG).show();
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance("https://pipiceapp-default-rtdb.europe-west1.firebasedatabase.app/");
+        DatabaseReference reference = database.getReference().child("languages");
 
 
         reference.addValueEventListener(new ValueEventListener() {
+            @SuppressLint("SetTextI18n")
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                String value = snapshot.getValue(String.class);
+                //String value = snapshot.getValue();
 
-                Log.d(TAG, "Value is: " + value);
-                Toast.makeText(MainActivity.this, value, Toast.LENGTH_LONG).show();
+                //Log.d(TAG, "Value is: " + value);
+                //Toast.makeText(MainActivity.this, value, Toast.LENGTH_LONG).show();
+
+                textView.setText("Text is: " + snapshot.getValue());
             }
 
             @Override
