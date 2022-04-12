1304d22c681319e09b649358a59bd142d67bca01
Hrvoje Kokosarevic <hrvoje.kok@gmail.com>
2022-04-12 22:20:01 +0200

    dodan binding

--- app/src/main/java/com/example/pipiceapp/MainActivity.java
+++ app/src/main/java/com/example/pipiceapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.pipiceapp;
 
 import static android.content.ContentValues.TAG;
+import static android.widget.Toast.makeText;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,65 +9,70 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.pipiceapp.databinding.ActivityMainBinding;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.sql.Array;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
 
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         TextView textView = findViewById(R.id.text1);
         ListView listView = findViewById(R.id.listView);
 
-        ArrayList<String> arrayList = new ArrayList<>();
+        ArrayList<Item> arrayList = new ArrayList<>();
 
-        arrayList.add("andorid");
-        arrayList.add("ando2rid");
-        arrayList.add("and3orid");
-        arrayList.add("ando24rid");
-        arrayList.add("ando45rid");
-        arrayList.add("andorid");
-        arrayList.add("ando2rid");
-        arrayList.add("and3orid");
-        arrayList.add("ando24rid");
-        arrayList.add("ando45rid");
 
         ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList);
         listView.setAdapter(arrayAdapter);
 
 
-        //Toast.makeText(MainActivity.this, "Firebase connected", Toast.LENGTH_LONG).show();
 
         FirebaseDatabase database = FirebaseDatabase.getInstance("https://pipiceapp-default-rtdb.europe-west1.firebasedatabase.app/");
-        DatabaseReference reference = database.getReference().child("languages");
+        DatabaseReference reference = database.getReference().child("mobiteli");
 
+        ListAdapter listAdapter = new ListAdapter(MainActivity.this, arrayList);
+
+        binding.listView.setAdapter(listAdapter);
+        binding.listView.setClickable(true);
+        binding.listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                makeText(MainActivity.this, "You clicked", Toast.LENGTH_LONG).show();
+            }
+        });
 
         reference.addValueEventListener(new ValueEventListener() {
             @SuppressLint("SetTextI18n")
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                //String value = snapshot.getValue();
 
-                //Log.d(TAG, "Value is: " + value);
-                //Toast.makeText(MainActivity.this, value, Toast.LENGTH_LONG).show();
 
-                textView.setText("Text is: " + snapshot.getValue());
+                int numberOfChildren = (int) snapshot.getChildrenCount();
+                int i;
+                for(i = 0; i < numberOfChildren; i++){
+                    textView.setText("Text is: " + snapshot.child("Samsung Galaxy S10").child("store").child("ekupi").getValue());
+                }
             }
 
             @Override
