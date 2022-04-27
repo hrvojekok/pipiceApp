4411cbd704a71f85fad322415629e9f03addf916
Hrvoje Kokosarevic <hrvoje.kok@gmail.com>
2022-04-27 21:53:35 +0200

    ispravljen bug

--- app/src/main/java/com/example/pipiceapp/DetailsActivity.java
+++ app/src/main/java/com/example/pipiceapp/DetailsActivity.java
@@ -40,54 +40,8 @@ public class DetailsActivity extends AppCompatActivity {
 
         Intent intent = this.getIntent();
 
-
-        String[] firstPrice = {"5899", "9001", "9955", "11223", "4334", "5004", "6000", "7000", "7877"};
-        String[] secondPrice = {"5987", "8898", "9932", "11993", "4455", "4999", "6550", "6988", "8877"};
-        String[] thirdPrice = {"5999", "8992", "9928", "11099", "5000", "5110", "6554", "6779", "7663"};
         String[] phoneNameList = {"Samsung Galaxy S10", "Samsung Galaxy S20", "Samsung Galaxy S21", "Samsung Galaxy S22", "Samsung Galaxy S5", "Samsung Galaxy S6",
                 "Samsung Galaxy S7", "Samsung Galaxy S8", "Samsung Galaxy S9"};
-
-
-
-
-        if(intent.getStringExtra("phoneName") != null) {
-
-            String phoneName = intent.getStringExtra("phoneName");
-
-            int index = Arrays.asList(phoneNameList).indexOf(phoneName);
-            String itemIDString = intent.getStringExtra("itemNumber");
-            //Toast.makeText(DetailsActivity.this, "i: " + itemIDString, Toast.LENGTH_LONG).show();
-
-
-
-            textView2.setText(firstPrice[index]);
-            textView3.setText(secondPrice[index]);
-            textView4.setText(thirdPrice[index]);
-
-
-
-            //textView5.setText(itemIDString);
-
-            //int itemNumber = Integer.parseInt(intent.getStringExtra("itemNumber"));
-            //String firstPriceString = firstPrice[itemNumber];
-            //String secondPriceString = secondPrice[itemNumber];
-            //String thirdPriceString = thirdPrice[itemNumber];
-
-            String imageID = intent.getStringExtra("imageID");
-            //Toast.makeText(DetailsActivity.this, "image id" + imageID, Toast.LENGTH_LONG).show();
-
-            SharedPreferences sh = getSharedPreferences("SharedPreferences", Context.MODE_MULTI_PROCESS);
-            String a = sh.getString("index", "");
-            Toast.makeText(DetailsActivity.this, "image id" + a, Toast.LENGTH_LONG).show();
-
-            textView1.setText(a);
-            //textView2.setText(firstPriceString);
-            //textView3.setText(secondPriceString);
-            //textView4.setText(thirdPriceString);
-        }
-
-
-
 
 
         FirebaseDatabase database = FirebaseDatabase.getInstance("https://pipiceapp-default-rtdb.europe-west1.firebasedatabase.app/");
@@ -119,18 +73,5 @@ public class DetailsActivity extends AppCompatActivity {
 
             }
         });
-
-
-//        if(intent.getStringExtra("itemNumber") != null) {
-//            int itemNumber = Integer.parseInt(intent.getStringExtra("itemNumber"));
-//            String firstPriceString = firstPrice[itemNumber];
-//            String secondPriceString = secondPrice[itemNumber];
-//            String thirdPriceString = thirdPrice[itemNumber];
-//
-//            //textView1.setText(phoneName);
-//            textView2.setText(firstPriceString);
-//            textView3.setText(secondPriceString);
-//            textView4.setText(thirdPriceString);
-//        }
     }
 }
